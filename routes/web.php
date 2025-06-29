7c263ccc3ddc9a61d5ebca5e43369fc55a68cae1
Ryanjawa <corazon3663@gmail.com>
2025-06-29 11:04:23 +0700

    modif welcome dan routes web

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('home', function () {
+    return view('home');
+});
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('shop', function () {
+    return view('shop');
+});
