232ccfc4534cd8dc7c9315df145a235533342140
Ryanjawa <corazon3663@gmail.com>
2025-06-29 15:02:31 +0700

    ganti template

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
-Route::get('home', function () {
-    return view('home');
-});
-Route::get('about', function () {
-    return view('about');
+Route::get('profile', function () {
+    return view('profile');
 });
 Route::get('shop', function () {
     return view('shop');
+});
+Route::get('about', function () {
+    return view('about');
 });
