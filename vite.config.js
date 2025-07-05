cec0337bff66a75451bb68733fcd5d8b269c4a4f
Ryanjawa <corazon3663@gmail.com>
2025-07-05 08:09:29 +0700

    konfigurasi penempatan template boostrape yang benar

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/argon-dashboard.min.css",
+                "resources/js/argon-dashboard.min.js",
+            ],
             refresh: true,
         }),
         tailwindcss(),
